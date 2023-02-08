43f501f853876a1e88fc1d916f8c416bbac39c36
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-02-08 15:56:33 +0100

    scroll

--- pages/object/[object_id].js
+++ pages/object/[object_id].js
@@ -12,14 +12,60 @@ const Object = ({ object }) => {
     <Layout>
       {/* <Seo seo={homepage.attributes.seo} /> */}
       <div className="object-page">
-        <h1>{object.title}</h1>
-        {object.subtitle &&<p className="small">{object.subtitle}</p>}
-        <span className="small">Year: {object.year}</span>
-        {object.cover_image.data &&
-          <div className="cover-image">
-            <Image image={object.cover_image.data}/>
+        <div className="flex">
+          <div className="main-info">
+            <span className="small">{object.object_id}</span>
+            <h1>{object.title}</h1>
+            {object.subtitle &&<h3>{object.subtitle}</h3>}
+            <div className="small-info">
+              {object.year &&<span className="small">Year: {object.year}</span>}
+              {object.type &&<span className="small">Type: {object.type.data.attributes.type}</span>}
+              {object.Relations.map((item, i) => {
+                return(
+                  <>
+                    {item.first_name &&
+                      <span className="small">{item.person_role.data.attributes.role}: {item.first_name} {item.last_name}</span>
+                    }
+                    {item.organisation &&
+                      <span className="small">{item.person_role.data.attributes.role}: {item.organisation}</span>
+                    }
+                  </>
+                )
+              })}
+            </div>
           </div>
-        }
+          <div className="image">
+            {object.cover_image.data &&
+              <div className="cover-image">
+                <Image image={object.cover_image.data}/>
+              </div>
+            }
+          </div>
+        </div>
+        <div className="scroll">
+          {object.content?.data?.map((item, i) =>{
+            return(
+              <div className='image'>
+                <Image image={item}/>
+              </div>
+            )
+          })}
+          {object.colophon?.data?.map((item, i) =>{
+            return(
+              <div className='image'>
+                <Image image={item}/>
+              </div>
+            )
+          })}
+          {object.spines?.data?.map((item, i) =>{
+            return(
+              <div className='image'>
+                <Image image={item}/>
+              </div>
+            )
+          })}
+          {object.back_cover.data && <div className='image back-cover'><Image image={object.back_cover.data}/></div>}
+        </div>
       </div>
     </Layout>
   )
@@ -27,7 +73,7 @@ const Object = ({ object }) => {
 
 export async function getServerSideProps({params}) {
   
-    const objectRes = await fetchAPI(`/objects?&filters[object_id][$eq]=${params.object_id}&populate=*`);
+    const objectRes = await fetchAPI(`/objects?&filters[object_id][$eq]=${params.object_id}&populate[Relations][populate]=*&populate[cover_image][populate]=*&populate[back_cover][populate]=*&populate[spines][populate]=*&populate[colophone][populate]=*&populate[content][populate]=*&populate=*`);
     
   
     return {
