6051bf573d06e62ed2e244821a1042f629be7cde
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-04-16 17:46:10 +0200

    filter modal css

--- components/nav.js
+++ components/nav.js
@@ -1,70 +1,23 @@
 import React, {useState} from "react"
 import Link from "next/link"
-import Modal from 'react-modal';
 
 const Nav = () => {
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [modalIsOpen2, setIsOpen2] = useState(false);
 
   function openModal() {
-    setIsOpen(true);
-  }
+    var element = document.getElementById("modal1");
+    element.classList.toggle("open");
 
-  function closeModal() {
-    setIsOpen(false);
+    var element2 = document.getElementById("modal");
+    element2.classList.toggle("open");
   }
 
   function openModal2() {
-    setIsOpen2(true);
+    var element = document.getElementById("modal2");
+    element.classList.toggle("open");
+
+    var element2 = document.getElementById("show_more");
+    element2.classList.toggle("open");
   }
-
-  function closeModal2() {
-    setIsOpen2(false);
-  }
-
-  function afterOpenModal2() {
-    document.getElementById('filter_options').style.display = 'none';
-  }
-
-  function afterCloseModal2() {
-    document.getElementById('filter_options').style.display = 'flex';
-  }
-
-
-  const customStyles = {
-    overlay: {
-      backgroundColor: "transparent",
-      zIndex: '999',
-    },
-    content: {
-      width: 'fit-content',
-      height: 'fit-content',
-      backgroundColor: "transparent",
-      zIndex: '999',
-      top: '100px',
-      left: '1vw',
-      border: 'none',
-      padding: 0,
-    },
-  };
-
-  const customStyles2 = {
-    overlay: {
-      backgroundColor: "transparent",
-      zIndex: '999',
-    },
-    content: {
-      width: 'fit-content',
-      height: 'fit-content',
-      backgroundColor: "transparent",
-      zIndex: '999',
-      top: '225px',
-      left: '1vw',
-      border: 'none',
-      padding: 0,
-    },
-  };
-  
 
   return (
     <div>
@@ -81,98 +34,80 @@ const Nav = () => {
       </nav>
       <div className="filter-search">
         <div className="filter button" onClick={openModal}>Filter</div>
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          style={customStyles}
-        >
-          <div className="filter-modal columns">
-            <div className="column">
+        <div className="search button">Search</div>      
+      </div>
+      <div className="filter-menu" id="modal">
+        <div className="filter-modal columns" id="modal1">
+          <div className="column">
+            <span className="small">Designer</span>
+            <div className="filter-items">
               <span className="small">Designer</span>
-              <div className="filter-items">
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-              </div>
-            </div>
-            <div className="column">
-              <span className="small">Year</span>
-              <div className="filter-items">
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-              </div>
-            </div>
-            <div className="column">
-              <span className="small">Type</span>
-              <div className="filter-items">
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-              </div>
-            </div>
-          </div>
-          <div className="filter-options" id="filter_options">
-            <div className="button" onClick={openModal2}>More options</div>
-            <div className="button">Clear filters</div>
-          </div>
-        </Modal>
-
-        <Modal
-          isOpen={modalIsOpen2}
-          onRequestClose={closeModal2}
-          onAfterOpen={afterOpenModal2}
-          onAfterClose={afterCloseModal2}
-          style={customStyles2}
-        >
-          <div className="filter-modal2 columns">
-            <div className="column">
               <span className="small">Designer</span>
-              <div className="filter-items">
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-              </div>
-            </div>
-            <div className="column">
-              <span className="small">Year</span>
-              <div className="filter-items">
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-              </div>
-            </div>
-            <div className="column">
-              <span className="small">Type</span>
-              <div className="filter-items">
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-              </div>
-            </div>
-            <div className="column">
-              <span className="small">Type</span>
-              <div className="filter-items">
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-                <span className="small">Designer</span>
-              </div>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
             </div>
           </div>
-          <div className="filter-options">
-            <div className="button" onClick={closeModal2}>Fewer options</div>
-            <div className="button">Clear filters</div>
+          <div className="column">
+            <span className="small">Year</span>
+            <div className="filter-items">
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+            </div>
           </div>
-        </Modal>
-
-        <div className="search button">Search</div>
+          <div className="column">
+            <span className="small">Type</span>
+            <div className="filter-items">
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+            </div>
+          </div>
+        </div>
+        <div className="filter-modal2 columns" id="modal2">
+          <div className="column">
+            <span className="small">Designer</span>
+            <div className="filter-items">
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+            </div>
+          </div>
+          <div className="column">
+            <span className="small">Year</span>
+            <div className="filter-items">
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+            </div>
+          </div>
+          <div className="column">
+            <span className="small">Type</span>
+            <div className="filter-items">
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+            </div>
+          </div>
+          <div className="column">
+            <span className="small">Type</span>
+            <div className="filter-items">
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+              <span className="small">Designer</span>
+            </div>
+          </div>
+        </div>
+        <div className="filter-options" id="filter_options">
+          <div className="button" id="show_more" onClick={openModal2}>options</div>
+          <div className="button">Clear filters</div>
+        </div>
       </div>
     </div>
   )
